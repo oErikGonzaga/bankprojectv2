f1021e40a93f4ced91bb3650f1522c08da82a46f
oerikgonzaga <erikgonzaga@outlook.com>
2022-11-28 10:17:41 -0300

    Iniciando Projeto

---
+++ src/main/java/com/gonzaga/banksystemv2/BankSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.gonzaga.banksystemv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemV2Application { ,
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzaga/banksystemv2/BankSystemV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzaga.banksystemv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
