1c8d8d5ed1ec8e77cfc4546ab4a3187f27582958
Erik Gonzaga <erikgonzaga@pop-os.localdomain>
2022-12-05 09:57:01 -0300

    - Remocao de arquivos; - inclusao .gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# bankprojectv2
+touch
--- src/main/java/com/gonzaga/banksystemv2/BankSystemV2Application.java
+++ src/main/java/com/gonzaga/banksystemv2/BankSystemV2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankSystemV2Application { ,
+public class BankSystemV2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankSystemV2Application.class, args);
