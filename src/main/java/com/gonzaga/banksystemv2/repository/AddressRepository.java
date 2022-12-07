71e744e090a38e1eb0cda2099737cfcc87771fda
Erik Gonzaga <erikgonzaga@outlook.com>
2022-12-07 20:34:36 -0300

    Interfaces que herdam JpaRepository, que utilizamos para fazer CRUDs.

---
+++ src/main/java/com/gonzaga/banksystemv2/repository/AddressRepository.java
@@ -0,0 +1,18 @@
+package com.gonzaga.banksystemv2.repository;
+
+import com.gonzaga.banksystemv2.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+    Segunda "Classe", na verdade, "Interface".
+    Ela irá "extender / herdar" de outra "interface" no caso JpaRepository,
+    que possui os métodos que precisamos para fazer um CRUD (criar, ler, atualizar, deletar)
+    Herança é representada por extends,
+    seguida do nome da classe da qual se deseja herdar
+ */
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
---
+++ src/main/java/com/gonzaga/banksystemv2/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.gonzaga.banksystemv2.repository;
+
+import com.gonzaga.banksystemv2.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
