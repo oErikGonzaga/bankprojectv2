d1ea06f2215d05f8f15bb8642a70fa1c21dcfcbc
Erik Gonzaga <erikgonzaga@outlook.com>
2022-12-11 14:59:26 -0300

    Adicionando ao Controller: @RestController e @PostMapping.

--- src/main/java/com/gonzaga/banksystemv2/controllers/BankController.java
+++ src/main/java/com/gonzaga/banksystemv2/controllers/BankController.java
@@ -1,4 +1,24 @@
 package com.gonzaga.banksystemv2.controllers;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*  Os Controllers são nossas portas de Entrada,
+    nossos End Points, nossa porta de entrada da aplicação.
+    Um Controller é responsavel por:
+    enviar requests e receber responses */
+
+/*  @RestController: deixa nossa classe exposta,
+    para ser acessada via api (Postman para testes) */
+
+@RestController
 public class BankController {
+
+    // @PostMapping: cria um registro atraves do verbo Post (Http), através da URL de acesso.
+    @PostMapping("api/create")
+    public String createAccounte(){
+        return "Sucess";
+    }
+
 }
