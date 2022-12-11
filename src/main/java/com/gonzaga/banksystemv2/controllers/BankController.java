b533d1efd969be79af324f6d685e960ef4c67e89
Erik Gonzaga <erikgonzaga@outlook.com>
2022-12-11 15:06:21 -0300

    "Settando" código de Sucesso Default 201.

--- src/main/java/com/gonzaga/banksystemv2/controllers/BankController.java
+++ src/main/java/com/gonzaga/banksystemv2/controllers/BankController.java
@@ -1,7 +1,9 @@
 package com.gonzaga.banksystemv2.controllers;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /*  Os Controllers são nossas portas de Entrada,
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BankController {
 
     // @PostMapping: cria um registro atraves do verbo Post (Http), através da URL de acesso.
+    @ResponseStatus(HttpStatus.CREATED) // "Settando" código de Sucesso Default 201.
     @PostMapping("api/create")
     public String createAccounte(){
         return "Sucess";
