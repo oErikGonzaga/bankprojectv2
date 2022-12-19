eceafe37e7a2a1d52719cd801d4bfd014d6c4344
gonzaga <erikgonzaga@outlook.com>
2022-12-19 15:17:06 -0300

    Marcando uma classe com @Autowired.

--- src/main/java/com/gonzaga/banksystemv2/controllers/BankController.java
+++ src/main/java/com/gonzaga/banksystemv2/controllers/BankController.java
@@ -1,8 +1,14 @@
 package com.gonzaga.banksystemv2.controllers;
 
 
+import com.gonzaga.banksystemv2.models.Account;
+import com.gonzaga.banksystemv2.requests.AccountRequest;
+import com.gonzaga.banksystemv2.services.BankService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class BankController {
+
+    @Autowired // -> Marca a classe como um Java'Bean
+    private BankService bankService;
 
     // @PostMapping: cria um registro atraves do verbo Post (Http), através da URL de acesso.
     @ResponseStatus(HttpStatus.CREATED) // "Settando" código de Sucesso Default 201.
