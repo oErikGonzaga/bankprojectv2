5a96e7660ec3621d70d5e0536250bc02220e500d
Erik Gonzaga <erikgonzaga@outlook.com>
2022-12-19 17:01:55 -0300

    Preparando a classe para receber as requests e contruir os repositórios.

--- src/main/java/com/gonzaga/banksystemv2/services/impl/BankServiceImpl.java
+++ src/main/java/com/gonzaga/banksystemv2/services/impl/BankServiceImpl.java
@@ -1,19 +1,41 @@
 package com.gonzaga.banksystemv2.services.impl;
 
 import com.gonzaga.banksystemv2.models.Account;
+import com.gonzaga.banksystemv2.models.Address;
+import com.gonzaga.banksystemv2.models.Client;
+import com.gonzaga.banksystemv2.repositories.AccountRepository;
+import com.gonzaga.banksystemv2.repositories.AddressRepository;
+import com.gonzaga.banksystemv2.repositories.ClientRepository;
 import com.gonzaga.banksystemv2.requests.AccountRequest;
 import com.gonzaga.banksystemv2.services.BankService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /* Essa classe e um bean, recebe um @Service
-para indicar que ela esta mantendo a logica de negocios */
+para indicar que ela esta mantendo a lógica de negócios */
 @Service
+@RequiredArgsConstructor
 public class BankServiceImpl implements BankService {
+
+    /* Quando implementamos essa classe,
+    automaticamente devemos adicionar os construtores usando @RequiredArgsConstructor
+    */
+
+    private final AccountRepository accountRepository;
+    private final AddressRepository addressRepository;
+    private final ClientRepository clientRepository;
+
     @Override
     public Account createAccount(AccountRequest request) {
 
-        Account account = new Account();
-        account.setNumber(Long.valueOf(request.getNumber()));
+        Account account = Account.builder().build();
+        Address address = Address.builder().build();
+        Client client = Client.builder().build();
+
+        Account accounSaved = accountRepository.save(account);
+        Address addressSaved = addressRepository.save(address);
+        Client clientSaved = clientRepository.save(client);
+
         return account;
     }
 }
