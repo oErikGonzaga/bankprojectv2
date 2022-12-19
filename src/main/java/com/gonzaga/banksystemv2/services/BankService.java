a13df7c5c1b1876c76f446a2661bcb5c9c54e9f9
gonzaga <erikgonzaga@outlook.com>
2022-12-19 15:18:56 -0300

    Criando uma Interface de Servicos, para criacao de conta.

--- src/main/java/com/gonzaga/banksystemv2/services/BankService.java
+++ src/main/java/com/gonzaga/banksystemv2/services/BankService.java
@@ -3,6 +3,10 @@ package com.gonzaga.banksystemv2.services;
 import com.gonzaga.banksystemv2.models.Account;
 import com.gonzaga.banksystemv2.requests.AccountRequest;
 
+/*  Uma Interface e uma classe Abstrata. Nao pode ser instanciada apenas herdada.
+Nao pode ter um objeto criado a partir de sua instanciacao.
+Ela serve pra que outras classes implemente seus metodos. */
+
 public interface BankService {
 
     Account createAccount(AccountRequest request);
